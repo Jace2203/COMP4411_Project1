8fbd8c2f64e2c65d988615ae87237731b044b166
Jace2203 <thomasw2203@gmail.com>
2022-02-20 01:27:58 +0800

    Avoid overread in edge clipping

--- EdgeClipping.cpp
+++ EdgeClipping.cpp
@@ -9,6 +9,13 @@ void EdgeClipping::clip(unsigned char* bmp, int width, int height, int x, int y,
     for (int i = 0; i > size[0]; i--)
     {
         int new_x = x + (int)(i * cos(angle)), new_y = y + (int)(i * sin(angle));
+
+        if (new_x < 0 || new_x >= width || new_y < 0 || new_y >=height)
+        {
+            size[0] = i;
+            break;
+        }
+
         if ((int)(bmp[(new_y * width + new_x) * 3]) !=  color)
         {
             size[0] = i;
@@ -19,6 +26,13 @@ void EdgeClipping::clip(unsigned char* bmp, int width, int height, int x, int y,
     for (int i = 0; i < size[1]; i++)
     {
         int new_x = x + (int)(i * cos(angle)), new_y = y + (int)(i * sin(angle));
+
+        if (new_x < 0 || new_x >= width || new_y < 0 || new_y >=height)
+        {
+            size[1] = i;
+            break;
+        }
+
         if ((int)(bmp[(new_y * width + new_x) * 3]) !=  color)
         {
             size[1] = i;
