e718fc7cf5df2c8a77984332136336e1a6ff39dc
Brianching522 <brianching522@gmail.com>
2022-02-24 03:10:31 +0800

    in progress

--- PaintView.cpp
+++ PaintView.cpp
@@ -10,6 +10,8 @@
 #include "paintview.h"
 #include "ImpBrush.h"
 #include "StrokeDirection.h"
+#include <math.h>
+#include <iostream>
 
 
 #define LEFT_MOUSE_DOWN		1
@@ -39,6 +41,16 @@ LinkedList::~LinkedList()
 {
 }
 
+Pointll::Pointll(int x, int y)
+{
+	xy = Point(x, y);
+	next = nullptr;
+}
+
+Pointll::~Pointll()
+{
+}
+
 static int		autopaint = 0;
 static int		autopaintspacing = 0;
 
@@ -52,6 +64,7 @@ PaintView::PaintView(int			x,
 	m_nWindowWidth	= w;
 	m_nWindowHeight	= h;
 	m_SavedPhoto = nullptr;
+	m_pllMax = nullptr;
 }
 
 
@@ -358,4 +371,140 @@ void PaintView::AutoPaint(int spacing)
 	autopaintspacing = spacing;
 	autopaint = 1;
 	redraw();
+}
+
+void PaintView::DrawPaintly()
+{
+	m_pDoc->setBrushType(2);
+	srand(time(NULL));
+
+	unsigned char* canvas = new unsigned char[m_nDrawWidth * m_nDrawHeight * 3];
+	memset (canvas, 0, m_nDrawWidth * m_nDrawHeight * 3);
+	unsigned char* sourceImage = m_pDoc->m_ucBitmap;
+	unsigned char* referenceImage;
+
+	int size = pow(2, m_pDoc->m_nPaintlyR0Level + 1);
+	for (int layer = 0; layer < m_pDoc->m_nPaintlyLayer; ++layer)
+	{
+		m_nNumMax = 0;
+		//m_pDoc->setSize(size);
+
+		/*apply blur - need to be added*/
+		referenceImage = sourceImage;
+
+		/*paintLayer*/
+		paintLayer(canvas, referenceImage, size);
+
+		Point* PointList = new Point[m_nNumMax];
+		Point temp_pt;
+		Pointll* head = m_pllMax;
+		int index;
+		for(int t = 0; t < m_nNumMax; ++t)
+		{
+			PointList[t] = head->xy;
+			head = head->next;
+		}
+
+		for(int t = 0; t < m_nNumMax; ++t)
+		{
+			index = rand() % m_nNumMax;
+			temp_pt = PointList[index];
+			PointList[index] = PointList[t];
+			PointList[t] = temp_pt;
+		}
+
+		glDrawBuffer(GL_FRONT_AND_BACK);
+		m_pDoc->setSize(size);
+		for(int t = 0; t < m_nNumMax; ++t)
+			m_pDoc->m_pCurrentBrush->BrushMove( PointList[t], PointList[t] );
+		glFlush();
+		SaveCurrentContent();
+		size /= 2;
+	}
+	
+
+	printf("finish");
+}
+
+void PaintView::paintLayer(unsigned char* canvas, unsigned char* referenceImage, int R)
+{
+	double* D = new double[m_nDrawWidth*m_nDrawHeight];
+	Point max;
+	GLubyte color1[3], color2[3];
+	double grid = m_pDoc->m_nPaintlyGridSize * R;
+
+
+	for (int y = 0; y < m_nDrawHeight; ++y)
+		for (int x = 0; x < m_nDrawWidth; ++x)
+		{
+			memcpy(color1, canvas + 3 * (x + y * m_nDrawWidth), 3);
+			memcpy(color2, referenceImage + 3 * (x + y * m_nDrawWidth), 3);
+			D[x+y*m_nDrawWidth] = ColorDiff(color1, color2);
+		}
+
+	for (int y = 0; y < m_nDrawHeight; y += grid)
+		for (int x = 0; x < m_nDrawWidth; x += grid)
+		{
+			if (AreaError(x, y, grid / 2, max, D) > m_pDoc->m_nPaintlyTheshold)
+			{
+				if (!m_pllMax)
+					m_pllMax = new Pointll(max.x, max.y);
+				else
+				{
+					Pointll* temp = m_pllMax;
+					while(temp->next)
+						temp = temp->next;
+					temp->next = new Pointll(max.x, max.y);
+				}
+				++m_nNumMax;
+				//printf("%d %d max: %d %d\n", x, y, max.x, max.y);
+			}
+		}
+}
+
+void PaintView::makeSplineStroke(int x_0, int y_0, int R, unsigned char* referenceImage)
+{
+}
+
+double PaintView::AreaError(int x, int y, double grid, Point& max, double* D)
+{
+	int upper_x = (x + grid > m_nDrawWidth) ? m_nDrawWidth : x + grid,
+		lower_x = (x - grid < 0) ? 0 : x - grid,
+		upper_y = (y + grid > m_nDrawHeight) ? m_nDrawHeight : y + grid,
+		lower_y = (y - grid < 0) ? 0 : y - grid;
+	double error = 0;
+	double nMax = 0;
+	max = Point(lower_x, lower_y);
+
+	for(int j = lower_y; j < upper_y; ++j)
+		for(int i = lower_x; i < upper_x; ++i)
+		{
+			error += D[i+j*m_nDrawWidth];
+			if (D[i+j*m_nDrawWidth] > nMax)
+			{
+				D[i + j * m_nDrawWidth];
+				max = Point(i, j);
+			}
+		}
+	return error / (4 * grid * grid);
+}
+
+double PaintView::ColorDiff(GLubyte* color1, GLubyte* color2)
+{
+	return sqrt(pow(*(color1) - *(color2), 2) + pow(*(color1+1) - *(color2+1), 2) + pow(*(color1+2) - *(color2+2), 2));
+}
+
+GLubyte* PaintView::GetColor(unsigned char* source, int x, int y )
+{
+	if ( x < 0 ) 
+		x = 0;
+	else if ( x >= m_nDrawWidth ) 
+		x = m_nDrawWidth-1;
+
+	if ( y < 0 ) 
+		y = 0;
+	else if ( y >= m_nDrawHeight ) 
+		y = m_nDrawHeight-1;
+
+	return (GLubyte*)(source + 3 * (y*m_nDrawHeight + x));
 }
--- PaintView.h
+++ PaintView.h
@@ -24,6 +24,15 @@ public:
 	LinkedList* next;
 };
 
+class Pointll
+{
+public:
+	Pointll(int x, int y);
+	~Pointll();
+	Point xy;
+	Pointll* next;
+};
+
 class PaintView : public Fl_Gl_Window
 {
 public:
@@ -44,6 +53,13 @@ public:
 	void LoadForUndo();
 	void AutoPaint(int spacing);
 
+	void DrawPaintly();
+	void paintLayer(unsigned char* canvas, unsigned char* referenceImage, int R);
+	double AreaError(int x, int y, double grid, Point& max, double* D);
+	double ColorDiff(GLubyte* color1, GLubyte* color2);
+	GLubyte* GetColor(unsigned char* source, int x, int y);
+	void makeSplineStroke(int x_0, int y_0, int R, unsigned char* referenceImage);
+
 	ImpressionistDoc *m_pDoc;
 
 private:
@@ -57,6 +73,8 @@ private:
 			m_nWindowWidth, 
 			m_nWindowHeight;
 	Point	m_pAutoPaintPoint;
+	Pointll* m_pllMax;
+	int		 m_nNumMax;
 
 	LinkedList* m_SavedPhoto;
 };
